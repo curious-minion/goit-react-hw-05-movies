e9635282af52c88020432938a9fd77e1cadd0322
courios-minion <okarskaolya@gmail.com>
2021-08-26 00:03:50 +0200

    almost done

--- src/components/MoviesPage/MoviesPage.js
+++ src/components/MoviesPage/MoviesPage.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, Fragment } from 'react';
 
 import { Link, useRouteMatch, useLocation, useHistory } from 'react-router-dom';
 import queryString from 'query-string';
@@ -8,67 +8,96 @@ import axios from 'axios';
 import links from '../../services/links';
 
 import Loader from 'react-loader-spinner';
+import routes from '../../routes';
+import MovieCard from '../HomePage/MovieCard';
+
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import '../Navigation/Navigation.scss';
+
+import './MoviesPage.scss';
+import '../HomePage/Homepage.scss';
 
 export default function MoviesPage() {
   const location = useLocation();
   const history = useHistory();
-  const { url } = useRouteMatch();
-  const { searchQuery } = queryString.parse(location.search);
-
   const [request, setRequest] = useState('');
+
+  const { query } = queryString.parse(location.search);
 
   const handleRequestChange = event => {
     setRequest(event.currentTarget.value.toLowerCase());
+  };
+
+  const handleFormSubmit = request => {
+    setRequest('');
+    if (request.length === 0) {
+      notify();
+      return;
+    }
+    setRequest(request);
+    refetch({ refetchPage: (page, index) => index === 0 });
   };
 
   const handleSubmit = event => {
     event.preventDefault();
 
     if (request.trim() === '') {
-      alert('Error');
-      //   toast.error('Type in your search request');
+      toast.error('Type in your search request');
       return;
     }
-
+    handleFormSubmit(request);
     onSubmit(request);
-    setRequest('');
   };
 
-  const onSubmit = query => {
+  const onSubmit = search => {
     history.push({
-      pathname: location.pathname,
-      search: `query=${query}`,
+      ...location,
+      search: `query=${search}`,
     });
   };
 
-  const fetchMovies = ({ nextPage = 1 }) => {
-    const response = axios(links.searchMovies(searchQuery, nextPage));
-    return {
-      data: response,
-      nextPage: nextPage + 1,
-    };
+  const fetchMovies = async ({ pageParam = 1 }) => {
+    const data = await axios(links.searchMovies(request, pageParam));
+
+    // setRequest('');
+    return data;
   };
+
+  // console.log(request);
+  // console.log(query);
 
   const {
     data,
     error,
+    isLoading,
+    isSuccess,
+    isError,
     fetchNextPage,
+    hasNextPage,
     isFetching,
     isFetchingNextPage,
     status,
-    hasNextPage,
+    refetch,
   } = useInfiniteQuery('movies', fetchMovies, {
-    getNextPage: (lastGroup, allGroups) => lastGroup.nextPage,
+    refetchOnWindowFocus: false,
+    enabled: false,
+    getNextPageParam: (lastPage, pages) => {
+      if (lastPage.data.page < pages[0].data.total_pages)
+        return Number(lastPage.data.page + 1);
+      return false;
+    },
   });
 
-  const paginatedData = [];
-  data.forEach(page => {
-    page.data.forEach(char => {
-      paginatedData.push(char);
-    });
-  });
+  const notify = () => {
+    toast.error('Whoops, something went wrong');
+    toast.clearWaitingQueue();
+    return;
+  };
 
-  console.log(paginatedData);
+  if (isLoading)
+    return <Loader type="ThreeDots" color="#00BFFF" height={60} width={60} />;
+  if (error) return notify();
 
   return (
     <>
@@ -90,40 +119,50 @@ export default function MoviesPage() {
           />
         </form>
       </header>
+
+      <main>
+        {isSuccess &&
+          data.pages.map((group, index) => (
+            <ul key={index} className="movies_list">
+              {isSuccess &&
+                group.data.results.map(
+                  ({ id, title, name, poster_path, vote_average }) => (
+                    <li key={id} className="movies_item">
+                      <Link
+                        to={{
+                          pathname: `${routes.movies}/${id}`,
+                          state: { from: location },
+                        }}
+                        className="movie_link"
+                      >
+                        <MovieCard
+                          name={title ?? name}
+                          image={poster_path}
+                          rating={vote_average}
+                        />
+                      </Link>
+                    </li>
+                  ),
+                )}
+            </ul>
+          ))}
+
+        {isSuccess && (
+          <button
+            onClick={() => fetchNextPage()}
+            disabled={!hasNextPage || isFetchingNextPage}
+            className="go_back load"
+          >
+            {isFetchingNextPage
+              ? 'Loading more...'
+              : hasNextPage
+              ? 'Load More'
+              : 'Nothing more to load'}
+          </button>
+        )}
+      </main>
+
+      <ToastContainer autoClose={2000} limit={1} />
     </>
   );
-
-  //     return status === 'loading' ? (
-  //      <Loader type="ThreeDots" color="#00BFFF" height={60} width={60} />
-  //    ) : status === 'error' ? (
-  //      <p>Error: {error.message}</p>
-  //    ) : (
-  //      <>
-  //        {data.pages.map((group, i) => (
-  //          <React.Fragment key={i}>
-  //            {group.projects.map(project => (
-  //              <p key={project.id}>{project.name}</p>
-  //            ))}
-  //          </React.Fragment>
-  //        ))}
-  //        <div>
-  //          <button
-  //            onClick={() => fetchNextPage()}
-
-  //            disabled={!hasNextPage || isFetchingNextPage}
-  //          >
-  //            {isFetchingNextPage
-  //              ? 'Loading more...'
-  //              : hasNextPage
-  //              ? 'Load More'
-  //              : 'Nothing more to load'}
-  //          </button>
-
-  //        </div>
-
-  //        <div>{isFetching && !isFetchingNextPage ? 'Fetching...' : null}</div>
-
-  //      </>
-
-  //    )
 }
